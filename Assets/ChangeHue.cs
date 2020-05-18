7d4cb1a0cb0008906be1659cd019675aefbb4647
Matheus Frantz <matheus_ftz@hotmail.com>
2020-05-18 19:05:17 -0300

    postprocess

---
+++ Assets/ChangeHue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class ChangeHue : MonoBehaviour
+{
+    public PostProcessVolume volume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
