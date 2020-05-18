c6dc72c43c65544741c9f694b66f94599ddf2540
Guilherme Sehna <guilhermesehna@hotmail.com>
2020-05-18 17:28:04 -0300

    Spaceship Movement

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -4,5 +4,34 @@ using UnityEngine;
 
 public class Spaceship : MonoBehaviour
 {
+    public float movementSpeed;
+    public float horizontalLimit;
+    public float shotDelay;
 
+    private void Update()
+    {
+        // Move Left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Move(-movementSpeed * Time.deltaTime);
+        }
+        // Move Right
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Move(movementSpeed * Time.deltaTime);
+        }
+    }
+
+    private void Move(float movement)
+    {
+        transform.Translate(new Vector2(movement, 0));
+        if (transform.position.x < -horizontalLimit)
+        {
+            transform.position = new Vector2(-horizontalLimit, transform.position.y);
+        }
+        else if (transform.position.x > horizontalLimit)
+        {
+            transform.position = new Vector2(horizontalLimit, transform.position.y);
+        }
+    }
 }
